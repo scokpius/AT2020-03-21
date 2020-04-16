00ce6d0a6e9b7401bea52d8aa1885d65f6a013e8
scokpius <scokpius@gmail.com>
2020-04-16 12:01:55 +0300

    added class constructor for two variables

--- src/homework_04/entity/Test.java
+++ src/homework_04/entity/Test.java
@@ -6,18 +6,18 @@ import homework_04.worker.AutomationEngineer;
 import homework_04.worker.Engineer;
 import homework_04.worker.TestEngineer;
 
-public class Test implements Function<Engineer, Result> {
+public abstract class Test implements Function<Engineer, Result> {
     private int complexity;
 
     private int instability;
 
-    Test(TestLevel testLevel) {
+    Test(TestLevel testLevel, int instability) {
         this.complexity = testLevel.COMPLEXITY;
+        if (instability == 0) this.instability = 1;
+        else if (instability > 10) this.instability = 10;
+        else this.instability = instability;
     }
 
-      void setInstability(int instability) {
-        this.instability = instability;
-    }
 
     @Override
     public Result apply(Engineer engineer) {
@@ -27,7 +27,7 @@ public class Test implements Function<Engineer, Result> {
             anxiety = engineer.getAnxiety();
         } else anxiety = 1;
 
-        if (complexity * instability * anxiety > 30) return Result.FAILED;
+        if (complexity * instability * anxiety/engineer.getSkill() > 30) return Result.FAILED;
         else return Result.PASSED;
     }
 }
