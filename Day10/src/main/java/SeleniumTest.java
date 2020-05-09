4fe809345cd14441198dff2266f01ada957423b7
scokpius <scokpius@gmail.com>
2020-05-10 02:52:07 +0300

    change code to display temperature

--- Day10/src/main/java/SeleniumTest.java
+++ Day10/src/main/java/SeleniumTest.java
@@ -6,13 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
 
-    static WebDriver driver = new ChromeDriver();
+    private static WebDriver driver = new ChromeDriver();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "c:\\students\\AT2020-03-21\\Day10\\src\\resourse\\chromedriver");
-//        System.setProperty("webdriver.chrome.silentOutput", "true"); //Timeout does not clean
-        // driver.get("https://selenium.dev");
-        driver.get("https://google.com");
+        System.setProperty("webdriver.chrome.silentOutput", "true"); //Timeout does not clean
+//        driver.get("https://selenium.dev");
 //        System.out.println(driver.getTitle());
 //        System.out.println(driver.getCurrentUrl());
 //        driver.navigate().to("https://ya.ru");
@@ -22,14 +21,20 @@ public class SeleniumTest {
 //        driver.navigate().refresh();
 //        System.out.println(driver.getTitle());
 //        System.out.println(driver.getCurrentUrl());
+        driver.get("https://google.com");
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("Погода Минск");
         element.submit();
-        WebElement element1 = driver.findElement(By.xpath("//*[@id=\'tsf\']/div[2]/div[1]/div[2]/div/div[2]/input"));
-        element1.clear();
-        element1.sendKeys("Погода на завтра");
-        element1.submit();
-        System.out.println(driver.findElement(By.xpath("//*[@id=\'wob_tm\']")));
+        Thread.sleep(1000);
+        WebElement element1 = driver.findElement(By.xpath("//*[@id='wob_dp']/div[2]"));
+        element1.click();
+        String weatherDay = driver.findElement(By.xpath("//*[@id='wob_dp']/div[2]/div")).getAttribute("aria-label");
+       // String weatherGrad = driver.findElement(By.cssSelector("svg[id='wob_gsvg'] > text[aria-label$='12:00']")).getText(); // тоько если текст видимый
+        String path = "svg[id='wob_gsvg'] > text[aria-label$='"+weatherDay+" 12:00']";
+        String weather = driver.findElement(By.cssSelector(path)).getAttribute("aria-label");
+        String[] weatherGrad = weather.split(" ");
+        System.out.println(String.format(" День недели: %s.\n Время определения температуры: 12:00.\n Температура: %s",
+                weatherDay, weatherGrad[0]));
         driver.quit();
     }
 }
--- myscript.js
+++
@@ -1,9 +0,0 @@
-var Eelement;
-
-function hideValue(x){
-	Eelement = x.value;
-	x.value="";
-};
-function  setValue(x){
-	x.value=Eelement;
-}
